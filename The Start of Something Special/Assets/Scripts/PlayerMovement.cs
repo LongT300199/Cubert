0fc6624fc15d6e72835dff5ffb4b3560611496e3
Long Thai <n10006257@qut.edu.au>
2019-05-04 16:00:58 +1000

    Added obstacles, fog, and drag

---
+++ The Start of Something Special/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public Rigidbody rb;
+
+	public float forwrardForce;
+	public float sidewayForce;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () { // when dealing with physics, use fixedupdate
+		bool moveLeft;
+		bool moveRight;
+
+		rb.AddForce(0, 0, forwrardForce * Time.deltaTime); // multiply by time.delta time to help fix difference in framerate updates
+
+		
+		//movement that fixes issues with movement not being detected due to low framerate
+		if (Input.GetKey("d")) {
+			moveRight = true;
+		} else { moveRight = false; }
+
+		if (Input.GetKey("a")) {
+			moveLeft = true;		
+		} else { moveLeft = false; }
+
+		if (moveRight == true) { rb.AddForce(sidewayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange); }
+		if (moveLeft == true) { rb.AddForce(-sidewayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange); }
+
+	}
+		
+}
